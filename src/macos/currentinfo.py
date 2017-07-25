3ab46de289f680730353b1f266d80762b9f03637
Brindal Patel <bittu@Brindals-MacBook-Pro.local>
2017-07-25 01:26:07 -0700

    updated the function to show wifi info in dictionory for mac

--- src/macos/currentinfo.py
+++ src/macos/currentinfo.py
@@ -6,12 +6,18 @@ def show_current_wifi(base_command):
     temp_result = os.popen(enter_command)
     result = temp_result.read()
     temp_result.close()
-    print result
-    # for i in result:
-    #     print i + "__end of the line"
-    # return result
+    result = result.replace(" ", "").splitlines( )
 
+    wifi_result = {}
+    for i in result:
+        if ("SSID" or "BSSID") in i:
+            wifi_result[i.split(":")[0]] = i.split(":",1)[-1]
+        elif "lRSSI" in i:
+            wifi_result["signal_strength"] = i.split(":")[-1]
 
+    print (wifi_result)
+
+# example output
 #       agrCtlRSSI: -72
 #      agrExtRSSI: 0
 #     agrCtlNoise: -95
