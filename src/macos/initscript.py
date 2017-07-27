261223867317a3433b3badf7151add47d7721222
bittu072 <bittu.patel072@gmail.com>
2017-07-26 22:55:09 -0700

    added the function to create xml file Created the function which creates the xml file from template file, and put into the given directory. In mac os, this the path is going to be fixed. Also, update the path of the script at the place of **script** and make it more dynamic

--- src/macos/initscript.py
+++ src/macos/initscript.py
@@ -1,6 +1,17 @@
 import os
 
-xml_path = os.path.dirname(os.path.abspath(__file__))
+base_path = os.path.dirname(os.path.abspath(__file__))
 
 def place_xml(path):
-    pass
+    xml_path = base_path + "/templates.d/starttemp.xml"
+    new_file = open(path, 'w')
+    with open(xml_path) as f:
+        for line in f:
+            if "**title**" in line:
+                new_file.write("\t\tThis is test\n")
+            elif "**script**" in line:
+                new_file.write("\t\t\tthis/is/path/of/script\n")
+            else:
+                new_file.write(line)
+    new_file.close()
+    print "done"
--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from macos.currentinfo import show_current_wifi
+from macos.initscript import place_xml
 
 base_command = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
 result = show_current_wifi(base_command)
 # print result
+
+place_xml("/Volumes/Bittu/trial.xml")
+print ("writing succedded")
