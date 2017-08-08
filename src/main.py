debb11580456e2cd86abbf87e91da674e5b00ea8
bittu072 <bittu.patel072@gmail.com>
2017-08-08 00:42:34 -0700

    corrected directory name and file permissions for plists xml launchd file

--- src/main.py
+++ src/main.py
@@ -5,12 +5,12 @@ from macos.installer.initscript import place_xml
 base_command = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
 result = show_current_wifi(base_command)
 
-xml_path = "/System/Library/LaunchDaemons/zerop"
+xml_path = "/System/Library/LaunchAgents/com.zerop.crontabtest.plist"
 # replace the app_path with dynamic path of the main application
 place_xml(xml_path, "app_path")
 
 # change permissin of the file
-os.chmod(xml_path, 0777)
+os.chmod(xml_path, 0644)
 # cahnge owner/group of the file
 
 print ("writing succedded")
