57cd87e2e4ce3b3cbd66028435746c7733876af2
bittu072 <bittu.patel072@gmail.com>
2017-08-08 00:28:53 -0700

    Updated code to make plist launchd working
    Tested code to place the xml file and also, updated the template file of xml, removed some comments

--- src/macos/installer/initscript.py
+++ src/macos/installer/initscript.py
@@ -2,17 +2,14 @@ import os
 
 base_path = os.path.dirname(os.path.abspath(__file__))
 
-def place_xml(path):
+def place_xml(path, app_path):
     xml_path = base_path + "/templates.d/starttemp.xml"
     new_file = open(path, 'w')
     with open(xml_path) as f:
         for line in f:
-            if "**title**" in line:
-                # instead of trial make it more efficient and dynamic name
-                new_file.write("\t<string>trial</string>\n")
-            elif "**script**" in line:
-                # confirm how to get static path
-                new_file.write("\t\t<string>" + path +"/zeromac.app</string>\n")
+            if "**script**" in line:
+                # confirm how to get dynamic path for the application main file
+                new_file.write("\t\t<string>" + app_path +"/zeromac.app</string>\n")
             else:
                 new_file.write(line)
     new_file.close()
--- src/main.py
+++ src/main.py
@@ -4,11 +4,11 @@ from macos.installer.initscript import place_xml
 
 base_command = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
 result = show_current_wifi(base_command)
-# print result
 
-# xml_path = "/System/Library/LaunchDaemons/zeromac.plist"
 xml_path = "/System/Library/LaunchDaemons/zerop"
-place_xml(xml_path)
+# replace the app_path with dynamic path of the main application
+place_xml(xml_path, "app_path")
+
 # change permissin of the file
 os.chmod(xml_path, 0777)
 # cahnge owner/group of the file
