b0b2860c9465a201878e35726281afa85c124ad9
bittu072 <bittu.patel072@gmail.com>
2017-08-08 23:33:26 -0700

    added the pathfinder file to get current user and copy the main app to the specific location

---
+++ src/macos/installer/pathfinder.py
@@ -0,0 +1,16 @@
+import getpass
+
+def show_current_user():
+    """ get the current user """
+    return getpass.getuser()
+
+# following line will give you a username of current login
+# print getpass.getuser()
+
+def copy_app():
+    """ copy the main application to another directory (in local machine) """
+    pass
+
+
+
+# https://stackoverflow.com/questions/22842016/launch-shell-script-on-login-in-mac-os-os-x
